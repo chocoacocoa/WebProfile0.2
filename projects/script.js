dae43d92eb6f793237eb97303606cd7d98dba7e1
chocoacocoa <vancholo2006@gmail.com>
2025-03-29 17:09:34 +0800

    hehe

--- projects/script.js
+++ projects/script.js
@@ -1,1 +1,7 @@
-console.log('js connected')
+console.log('js connected');
+let moon = document.getElementById('moon')
+
+// moon.addEventListener('animationend', function (){
+//     moon.style.animation = "animaMunRotate"
+    
+// })
