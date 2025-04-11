816e158b08faf49abb705819834a3f3b01953635
chocoacocoa <vancholo2006@gmail.com>
2025-04-11 19:44:34 +0800

    bookseeker palang nadadagdag pero di pa tapos

--- projects/script.js
+++ projects/script.js
@@ -1,7 +1,18 @@
 console.log('js connected');
 let moon = document.getElementById('moon')
+let pic1 = document.getElementById('pic1');
+let pic2 = document.getElementById('pic2');
+let pic3 = document.getElementById('pic3');
 
-// moon.addEventListener('animationend', function (){
-//     moon.style.animation = "animaMunRotate"
-    
-// })
+function processor(a){
+    a.style.transition = "1s";
+    a.addEventListener('mouseover', function(){
+        a.style.width  = "30vmin"
+    });
+    a.addEventListener('mouseout', function(){
+        a.style.width  = "unset"
+    });
+}
+processor(pic1);
+processor(pic2);
+processor(pic3);
